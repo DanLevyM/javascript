e734c3881593309c2af5028359864b30502527c7
DanLevyM <dan.levy.pro@gmail.com>
2022-05-30 15:38:41 +0200

    test ssh

--- array/array.js
+++ array/array.js
@@ -1,4 +1,4 @@
-const numbers = [1, 2, 3, 4];
+const numbers = [12, 2, 31, 4, 30, 25, 33, 100, 1];
 const sum = numbers.reduce((currentVal, nextVal) => currentVal + nextVal);
 console.log(sum);
 // 10
@@ -10,3 +10,21 @@ console.log(filter);
 const map = numbers.map(val => val * 2);
 console.log(map);
 // [2, 4, 6, 8]
+
+const sort = numbers.sort((a, b) => a - b);
+console.log(sort);
+
+const items = [
+  { name: "Edward", value: 21 },
+  { name: "Sharpe", value: 37 },
+  { name: "And", value: 45 },
+  { name: "The", value: -12 },
+  { name: "Magnetic", value: 13 },
+  { name: "Zeros", value: 37 }
+];
+const itemSort = items.sort(function (a, b) {
+  return a.value - b.value;
+});
+console.log(itemSort);
+
+
